be1de22f0dbe85a9936a23e2844813d413b78681
FranklinQH <quillifran@gmail.com>
2026-05-17 10:29:44 -0500

    se agrega el proyecto

---
+++ cuadrado.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        m=1*i;
+        cout<<endl<<"x"<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        cout<<"x";
+    }
+    return 0;
+}
